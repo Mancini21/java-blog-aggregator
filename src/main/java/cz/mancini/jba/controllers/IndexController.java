e42f5498e6850dcdbc4dc2e1e62c8f37e3d5906e
Mancini <mancini@iol.pt>
2014-12-29 00:35:52 +0100

    segundo commit

---
+++ src/main/java/cz/mancini/jba/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package cz.mancini.jba.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/index")
+	public String index(){
+		return "index";
+	}
+	
+
+}
